fd82b2285221670d21ec7a4851b3898713d33c98
asvindra <aswin1399904@gmail.com>
2021-12-17 22:41:10 +0530

    complted editor

---
+++ src/Output.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Output extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    let srcDoc = `
+      <html>
+        <body>${this.props.html}</body>
+        <style>${this.props.css}</style>
+        <script>${this.props.javaScript}</script>
+
+        
+      </html>
+      `;
+    return (
+      <div className='iframe-div'>
+        <iframe srcDoc={srcDoc} frameborder='0' title='output'></iframe>
+      </div>
+    );
+  }
+}
+
+export default Output;
